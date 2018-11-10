befa80ccb4e9f56c9a5c65034e74682b88f545ce
sunny <sunnyyorita@gmail.com>
2018-11-09 19:18:46 -0800

    added register, login, initial validation

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
     mongoURI:
-        'mongodb://sunny-yorita:vietdreamer1@ds153593.mlab.com:53593/yoritaconnector'
-}
+        'mongodb://sunny-yorita:vietdreamer1@ds153593.mlab.com:53593/yoritaconnector',
+    secretOrKey: 'secret'
+};
